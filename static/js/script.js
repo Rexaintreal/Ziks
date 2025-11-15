74b9462537d54620fb79e2e53b43af9746fe715e
Saurabh Tiwari <129385426+Rexaintreal@users.noreply.github.com>
2025-11-15 18:12:21 +0530

    added themes saving to local storage

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,19 @@
 const themeToggle = document.getElementById('themeToggle');
 const themeIcon = document.getElementById('themeIcon');
 const body = document.body;
-let currentTheme = 'dark';
-themeToggle.addEventListener('click', () => {
-    currentTheme = currentTheme === 'dark' ? 'light' : 'dark';
-    body.setAttribute('data-theme', currentTheme);
-    if (currentTheme === 'dark') {
-        themeIcon.className = 'fa-solid fa-sun';
+
+function applyTheme(theme) {
+    body.setAttribute('data-theme', theme);
+    if (theme === 'dark') {
+        themeIcon.className = 'fa-solid fa-sun'; 
     } else {
         themeIcon.className = 'fa-solid fa-moon';
     }
+}
+let currentTheme = localStorage.getItem('theme') || 'dark';
+applyTheme(currentTheme);
+themeToggle.addEventListener('click', () => {
+    currentTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    applyTheme(currentTheme);
+    localStorage.setItem('theme', currentTheme);
 });
